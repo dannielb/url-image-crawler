90a71195b86b8123bbc636518046ea3003c68f4f
Daniel Bonfim <dannielbomfim4@gmail.com>
2023-05-06 20:18:53 -0300

    initial commit

---
+++ lib/url_image_crawler.ex
@@ -0,0 +1,18 @@
+defmodule UrlImageCrawler do
+  @moduledoc """
+  Documentation for `UrlImageCrawler`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> UrlImageCrawler.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/url_image_crawler_test.exs
@@ -0,0 +1,8 @@
+defmodule UrlImageCrawlerTest do
+  use ExUnit.Case
+  doctest UrlImageCrawler
+
+  test "greets the world" do
+    assert UrlImageCrawler.hello() == :world
+  end
+end
