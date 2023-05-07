07c2909763933f4c5831cbf55f35f3aefa3ff219
Daniel Bonfim <dannielbomfim4@gmail.com>
2023-05-06 22:39:46 -0300

    update dependencies

--- mix.exs
+++ mix.exs
@@ -7,23 +7,43 @@ defmodule UrlImageCrawler.MixProject do
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: aliases(),
+      escript: escript()
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
-      mod: {UrlImageCrawler.Application, []}
+      extra_applications: [:logger]
     ]
+  end
+
+  defp escript do
+    [main_module: UrlImageCrawler.CLI]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:req, "~> 0.3.6"},
+      {:floki, "~> 0.34.2"},
+      {:mox, "~> 1.0.0", only: :test},
+      {:bypass, "~> 2.1", only: :test}
     ]
   end
+
+  # Aliases are shortcuts or tasks specific to the current project.
+  # For example, to install project dependencies and perform other setup tasks, run:
+  #
+  #     $ mix setup
+  #
+  # See the documentation for `Mix` for more info on aliases.
+  defp aliases do
+    [
+      setup: ["deps.get", "deps.compile"],
+    ]
+  end
+
 end
